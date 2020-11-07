aeb2bb9b29b0ef128202975c679b971ff1a88c90
Charles Ponti <cj@ponti.io>
2020-11-07 11:20:55 -0500

    (fix) use jest for twoSum test

--- problems/two-sum.js
+++ problems/two-sum.js
@@ -3,13 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+exports.twoSum = function(nums, target) {
     const map = new Map()
     
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i]
         
-        console.log(nums[i], diff, map.get(diff))
         if (map.get(diff) !== undefined) return [map.get(diff), i]
         
         map.set(nums[i], i)
@@ -17,5 +16,3 @@ var twoSum = function(nums, target) {
 
     return 'No two sum solution'
 };
-
-console.log(twoSum([2, 7, 11, 15],9))
